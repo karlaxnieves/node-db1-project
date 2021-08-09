7d334476e70ae939cee04311decf7b21cafd8ce2
Karla Nieves <karlaxnieves@gmail.com>
2021-08-09 13:25:44 -0700

    router is finished

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -31,20 +31,48 @@ router.post(
   }
 })
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.post(
+  '/', 
+  md.checkAccountNameUnique, 
+  md.checkAccountPayload, 
+  async (req, res, next) => {
+  try {
+    const newAccount = await Account.create({
+      name: req.body.name.trim(),
+      budget: req.body.budget,
+    })
+    res.status(201).json(newAccount)
+  }catch (err) {
+    next(err)
+  }
 })
 
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.put(
+  '/:id', 
+  md.checkAccountId,
+  md.checkAccountPayload,
+  async (req, res, next) => {
+  try {
+    const updated = await Account.updateById(req.params.id, req.body)
+    res.json(updated)
+  }catch (err) {
+    next(err)
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.delete('/:id', md.checkAccountId, async (req, res, next) => {
+  try {
+    await Account.deleteById(req.params.id)
+    res.json(req.account)
+  }catch (err) {
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message: err.message
+  })
 })
 
 module.exports = router;
