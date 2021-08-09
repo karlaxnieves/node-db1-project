b278591a67d9e29091735fe0ee48272dd79db4ba
Karla Nieves <karlaxnieves@gmail.com>
2021-08-09 13:20:59 -0700

    got started on the router

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,11 +1,34 @@
 const router = require('express').Router()
+const md = require('./accounts-middleware')
+const Account = require('./accounts-model')
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/', async (req, res, next) => {
+  try {
+    const accounts = await Account.getAll()
+    res.json(accounts)
+  } catch (err) {
+    next(err)
+  } 
 })
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/:id', md.checkAccountId, async (req, res, next) => {
+res.json(req.account)
+})
+
+router.post(
+  '/', 
+  md.checkAccountNameUnique, 
+  md.checkAccountPayload, 
+  async (req, res, next) => {
+  try {
+    const newAccount = await Account.create({
+      name: req.body.name.trim(),
+      budget: req.body.budget,
+    })
+    res.status(201).json(newAccount)
+  }catch (err) {
+    next(err)
+  }
 })
 
 router.post('/', (req, res, next) => {
